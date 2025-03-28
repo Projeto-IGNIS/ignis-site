fba2a864856a3b166465e510992f729380f4b7e4
ViniDias1 <viniciusdvalenca@gmail.com>
2025-03-28 10:53:18 -0300

    fix: update copyright year in Footer component to 2025

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,6 +1,6 @@
 const Footer = () => {
   return <footer className="py-4 text-center text-gray-400 text-sm">
-      <p>Copyrights 2024 Vinicius Dias</p>
+      <p>Copyrights 2025 Vinicius Dias</p>
     </footer>;
 };
 export default Footer;
