de479f6b1422c81f98a75021ba99c54d5caf6393
ViniDias1 <viniciusdvalenca@gmail.com>
2025-03-30 00:47:13 -0300

    refactor tailwind.config.js: add dark mode support and extend color theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,19 @@
-export default {content: [
-  './index.html',
-  './src/**/*.{js,ts,jsx,tsx}'
-],}
+export default {
+  darkMode: 'class',
+  content: [
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        light: {
+          background: '#f9f9f9',
+          text: '#4a4a4a', // Ajustado para um cinza mais escuro
+          primary: '#1d4ed8',
+          secondary: '#9333ea',
+        },
+      },
+    },
+  },
+};
