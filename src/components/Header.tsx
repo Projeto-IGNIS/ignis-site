46e7999bbba7cb6c8c45aecc01b13f02ecf982e4
ViniDias1 <viniciusdvalenca@gmail.com>
2025-03-31 15:16:03 -0300

    feat: update header component to include theme toggle icons and improve layout

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,22 +5,26 @@ import { useTheme } from '../context/ThemeContext';
 const Header = () => {
   const { theme, toggleTheme } = useTheme();
   return (
-    <header className="py-4 flex justify-between items-center">
-      <div>
-        <button
-          className="text-blue-400 font-semibold text-xl dark:text-blue-600"
-          aria-label="Home"
-        >
-          ViniDias
-        </button>
-      </div>
+    <header className="py-4 flex justify-end">
       <div className="flex items-center space-x-4">
         <button
           onClick={toggleTheme}
           className="text-gray-400 hover:text-blue-400 dark:text-gray-600 dark:hover:text-blue-500 transition-colors duration-200"
           aria-label="Toggle Theme"
         >
-          {theme === 'dark' ? '🌙' : '☀️'}
+          {theme === 'dark' ? (
+            <img
+              src="https://img.icons8.com/?size=100&id=TMEWD76XezdX&format=png&color=000000"
+              alt="Dark Mode Icon"
+              className="w-8 h-8"
+            />
+          ) : (
+            <img
+              src="https://img.icons8.com/?size=100&id=H3yHeysB1dxv&format=png&color=000000"
+              alt="Light Mode Icon"
+              className="w-8 h-8"
+            />
+          )}
         </button>
         <a href="https://www.linkedin.com/in/vinidias1" aria-label="LinkedIn" className="text-gray-400 hover:text-blue-400 dark:text-gray-600 dark:hover:text-blue-500 transition-colors duration-200">
           <LinkedinIcon size={18} />
